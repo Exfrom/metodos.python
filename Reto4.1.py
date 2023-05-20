226110a51a67bfcd1b96559d7b57495bfe78b389
AlejoMiran <josealejandromiranda00@gmail.com>
2023-05-20 11:20:19 -0500

    se modifica metodo lanzarmoneda

--- Reto4.1.py
+++ Reto4.1.py
@@ -1,21 +1,10 @@
 import random
-##juego carisellazo
-
-def main():
-    print("Bienvenido al juego del Carisellazo")
-    print("1.jugar: ")
-    print("2.salir")
-jugador = input("Ingrese el nombre del jugador: ")
-def lanzarMoneda():
-    if random.randint(0,1) == 0:
-        print("Has ganado")
-    else:
-        print("Has perdido")
+saldo = 1000  
     
+def lanzarMoneda():
+    jugador = input("Ingresa el nombre del jugador: ")
+    resultado = random.choice(["cara", "sello"])
+    return resultado
 
-
-
-    #moneda = ["cara", "sello"]
-    #resultado = random.choice(moneda)
-
+    
     
