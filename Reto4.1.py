9ab909507f5a5d266fe74cb01e61f7de2c112c44
Exfrom <dscelis@soy.sena.edu.co>
2023-05-27 15:24:42 -0500

    se termina reto 4.1

--- Reto4.1.py
+++ Reto4.1.py
@@ -1,13 +1,44 @@
 import random
-saldoinicial = 1000  
-jugador = input("Ingrese el nombre del jugador:  ")   
-def lanzarMoneda():
-    moneda=["cara", "sello"]
-    resultado = random.choice(moneda)
+
+saldo_global = 1000  # Saldo inicial o global
+
+def lanzar_moneda():
+    #Lanza una moneda y devuelve el resultado: cara o sello.
+    opciones = ["cara", "sello"]
+    resultado = random.choice(opciones)
     return resultado
 
-def ganar():
-    global saldoinicial
-    saldoinicial = saldoinicial + 1000
-    print("Has ganado")
+def ganar(monto, saldo):
+    #Aumenta el saldo del jugador al doble de la apuesta y muestra un mensaje de victoria.
+    saldo += monto * 2
+    print("¡Has ganado!", monto, "doblado. Saldo actual:", saldo)
+    return saldo
 
+def perder(monto, saldo):
+    #Reduce el saldo del jugador en el valor de la apuesta y muestra un mensaje de derrota.
+    saldo -= monto
+    print("Has perdido.", monto, "deducido. Saldo actual:", saldo)
+    return saldo
+
+def jugar():
+    #Permite al jugador elegir cara o sello y apostar un monto.
+    jugador = input("Ingrese el nombre del jugador: ")
+    saldo = saldo_global
+    while True:
+        print("Saldo actual:", saldo)
+        opcion = input("Elige cara o sello: ")
+        monto_apuesta = int(input("Ingrese el monto de la apuesta: "))
+        resultado = lanzar_moneda()
+        if resultado == opcion:
+            saldo = ganar(monto_apuesta, saldo)
+        else:
+            saldo = perder(monto_apuesta, saldo)
+
+        continuar = input("¿Deseas continuar jugando? (s/n): ")
+        if continuar.lower() != "s":
+            break
+
+    print("Juego terminado. ¡Gracias por jugar,", jugador + "! Tu saldo final es:", saldo)
+
+# Ejecutar el juego
+jugar()
