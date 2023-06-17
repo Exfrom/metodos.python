b214a666871e22098d83ba2658b455c21fed2b99
Exfrom <dscelis@soy.sena.edu.co>
2023-06-16 22:09:01 -0500

    secorrigecomentarios

--- colecciones/ejerciciolista.py
+++ colecciones/ejerciciolista.py
@@ -14,19 +14,19 @@ print(comida)
 
 for p, e in enumerate(comida):
     print("En la posicion" ,p, "Se encuentra el elemento" ,e)
-
+#insertar elemento
 comida.insert(2,"empanada")
 print(comida)
 
 comida.reverse()
 comida.sort(reverse=True)
-
+#borrar elementode la lista
 comida.remove("gaseosa")
 print(comida)
-
+#borrar un elemento de una posicion especifica
 del comida[4]
 print(comida)
-
+#borrar el ultimo elemento de la lista
 comida.pop()
 print(comida)
 #devolver en que posicion esta un elemento de la lista
